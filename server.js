8c8c6b436786cddc2fe8338fff1c265fc726f484
Ivan Telychkin <Ivan.Telichkin@Edetek.com>
2018-05-21 10:28:32 +0300

    Made some changes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 4200;
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(`${__dirname}/dist/observatory`));
+// app.use(express.static(`${__dirname}/dist/observatory`));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/observatory/index.html'));
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
