5fae53b239555560022a73766162e706dd1d4923
Ivan Telychkin <Ivan.Telichkin@Edetek.com>
2018-05-21 11:15:40 +0300

    Reverted to old libraries

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 4200;
 const app = express();
 
 // Serve only the static files form the dist directory
- app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(`${__dirname}/dist/observatory/index.html`));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
