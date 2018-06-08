39cab495e156cc9bce0d4f1fbe542563249eaae1
Ivan Telychkin <Ivan.Telichkin@Edetek.com>
2018-06-08 09:02:26 +0300

    Changed api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 
 export const environment = {
   production: false,
-  apiHost: 'https://observatory-server.herokuapp.com'
+  //apiHost: 'https://observatory-server.herokuapp.com'
+  apiHost: 'https://observatory-api-ts.herokuapp.com'
   // apiHost: 'http://localhost:3000'
 
 };
